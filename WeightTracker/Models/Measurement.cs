0126beae3395a9016fbc291bafda8cedc7c06679
mijenner <mijenner@gmail.com>
2024-09-30 19:44:22 +0200

    added Models w UserProfile and Measurement

---
+++ WeightTracker/Models/Measurement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeightTracker.Models
+{
+    class Measurement
+    {
+        public double Weight { get; set; } = 0.0;
+        public DateTime Date { get; set; } = DateTime.MinValue; 
+
+        public Measurement()
+        {
+        }
+
+        public Measurement(double weight, DateTime date)
+        {
+            Weight = weight;
+            Date = date;
+        }
+    }
+}
---
+++ WeightTracker/Models/UserProfile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeightTracker.Models
+{
+    class UserProfile
+    {
+        public string Name { get; set; } = string.Empty;
+        public double Height { get; set; } = 0;
+        public double Weight { get; set; } = 0;
+        public double RefWeight { get; set; } = 0;
+        public DateTime RefDate { get; set; } = DateTime.MinValue;
+
+        public UserProfile()
+        {            
+        }
+
+        public UserProfile(string name, double height)
+        {
+            Name = name;
+            Height = height;
+        }
+    }
+}
