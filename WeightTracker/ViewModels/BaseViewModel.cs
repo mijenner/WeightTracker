c348b0762c25e62f51e4d886620fe47591d18239
mijenner <mijenner@gmail.com>
2024-10-01 10:45:41 +0200

    base view model

---
+++ WeightTracker/ViewModels/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WeightTracker.ViewModels
+{
+    internal partial class BaseViewModel : ObservableObject  
+    {
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+        bool isBusy;
+        [ObservableProperty]
+        string title;
+        public bool IsNotBusy => !isBusy;
+    }
+}
